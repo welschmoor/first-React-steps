c92d170502d3015630a372603af1e30874b057fc
Johannes Welsch <welschmoor@gmail.com>
2021-09-14 23:11:22 +0200

    first ocmmit

---
+++ components/App.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+export function App() {
+    return (
+        <header>
+            <div>Wercome to SHITTY WOK</div>
+            <ul>
+                <li><a>ClickMe</a></li>
+                <li><a>ClickMe</a></li>
+                <li><a>ClickMe</a></li>
+            </ul>
+        </header>
+
+    )
+}
---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+
+export Footer() {
+    return (
+        <footer>Copyleft 2021</footer>
+    )
+}
