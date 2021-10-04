82d85be08acc5bb037ca989c9ffdfbc3c4210dbe
Johannes Welsch <welschmoor@gmail.com>
2021-10-04 13:36:00 +0200

    first one

--- components/App.js
+++ components/App.js
@@ -1,17 +1,37 @@
 
 
-import React from "react";
+import React from "react";  // we need it for JSX 
 
 export function App() {
-    return(
-        <div className="maindiv">
-            <h1 className="todo-titleh1">TO-DO LIST</h1>
-            <ul className="todo-ul">
-                <li className="todo-li"><span>V </span>Take out the trash</li>
-                <li className="todo-li"><span>V </span>Learn React</li>
-                <li className="todo-li"><span>V </span>Refresh ES6</li>
-                <li className="todo-li"><span>V </span>Buy food</li>
-            </ul>
-        </div>
+    const firstName = "Monkey"
+    const lastName = "Type"
+    const now = new Date();
+
+    let message;
+
+    if (now.getHours()> 12) message =`It's afternoon!`
+    else  message =`It's before noon!`
+
+
+
+    return (
+    <div>
+        <h1>Hello {firstName}</h1>
+        <h2>{ message }</h2>
+    </div>
+            
     )
-}
+} 
+
+
+{/* <div className="maindiv" style={style}>
+<h1 className="todo-titleh1">TO-DO LIST</h1>
+<ul className="todo-ul">
+    <li className="todo-li"><input type="checkbox" className="checkbox"/>Take out the trash</li>
+    <li className="todo-li"><input type="checkbox" className="checkbox"/>Learn React</li>
+    <li className="todo-li"><input type="checkbox" className="checkbox"/>Refresh ES6</li>
+    <li className="todo-li"><input type="checkbox" className="checkbox"/>Buy food</li>
+</ul>
+<p> {hours>12? 'afternoon': 'beforenoon'}</p>
+<p>Time: {hours}:{mins}</p>
+</div> */}
--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 'use strict';
 
-import React from "react";
+// Rock Papers Scissors by Johannes Welsch, 4 Oct. 2021
+// 
+
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-import {App} from './components/App.js';
+const availableMoves = [null, 'Rock', 'Paper', 'Scissors']
 
+const App = () => {
+    const [gameStartedS, setGameStartedS] = useState(false)
+    const [gameState, setGameState] = useState('')
 
+    const [playerChoiceS, setPlayerChoiceS] = useState(null)
+    const [compChoiceS, setCompChoiceS] = useState(null)
+    
+    // game logic
+    if (gameStartedS) {
+        if (playerChoiceS === compChoiceS ) setGameState('tie')
+        else if (playerChoiceS > compChoiceS && (Math.abs(playerChoiceS-compChoiceS)) < 2) setGameState('win')
+        else if  (playerChoiceS < compChoiceS && (Math.abs(playerChoiceS-compChoiceS)) > 1) setGameState('win') 
+        else setGameState('loss')            
+        setGameStartedS(false) 
+    }
+
+    const onClickHandler = (e) => {
+        e.preventDefault()
+        // comp choice. If we run right now, it will be undefined, rerender component first with gameStartedS
+        setCompChoiceS(Math.floor(Math.random()*3+1))
+        setPlayerChoiceS(+e.target.value)
+    
+        setGameStartedS(true)  
+    }
+
+    return(
+<div className="maindiv">
+    <div className="message">{gameState ? `It's a ${gameState}`: <span>&nbsp;</span>}</div>
+    <div className="playerchoices">{playerChoiceS ? availableMoves[playerChoiceS]: <span>&nbsp;</span>} : {compChoiceS ? availableMoves[compChoiceS]: <span>&nbsp;</span>}</div>
+    <form className="form">
+        <button type="submit" name="rock" value="1"  onClick={onClickHandler} >Rock</button>
+        <button type="submit" name="rock" value="2"  onClick={onClickHandler} >Paper</button>
+        <button type="submit" name="rock" value="3"  onClick={onClickHandler} >Scissors</button>
+    </form>
+</div>
+)}
 ReactDOM.render(<App/>, document.getElementById('root'));
